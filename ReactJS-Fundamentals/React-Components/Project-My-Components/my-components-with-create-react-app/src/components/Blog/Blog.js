18b28868784691b7633b53eac886d3a60f59ef19
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-10 17:26:40 +0300

    added blog component folder

---
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/Blog/Blog.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Blog.css';
+
+const Blog = () => {
+    return (
+        <section className="section blog-section border border-warning">
+            <div className='container'>
+                <div className='row border'>
+                    <h2>Blog Section!</h2>
+                </div>
+                <div className='row border p-5'></div>
+            </div>
+            <div className='row border'>
+                <button className='btn btn-lg btn-primary'>Learn More</button>
+            </div>
+        </section>
+    )
+};
+
+export default Blog;
