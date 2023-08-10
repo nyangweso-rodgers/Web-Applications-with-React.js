778569b3460e87e91228e64adc96c96492698e5e
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-10 17:53:24 +0300

    added components to app.js

--- ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import './App.css'
 import MyHeader from "./components/Header/MyHeader";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Blog from "./components/Blog/Blog";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
-        <div className="">
+        <div className="app">
             <MyHeader />
+            <About />
+            <Portfolio />
+            <Blog />
+            <Footer />
         </div>
     )
 };
