6f09b6a3782fbf5ee6d124fb8ae744ee7577d288
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-10 17:23:11 +0300

    added portfolio component folder

---
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Portfolio';
+
+const Portfolio = () => {
+    return (
+        <section className='section portfolio-section border border-info'>
+            <div className="container-fluid">
+                <div className='row border'>
+                    <h2>Portfolio Section!</h2>
+                </div>
+                <div className='row border p-5'></div>
+            </div>
+        </section>
+    )
+};
+
+export default Portfolio;
