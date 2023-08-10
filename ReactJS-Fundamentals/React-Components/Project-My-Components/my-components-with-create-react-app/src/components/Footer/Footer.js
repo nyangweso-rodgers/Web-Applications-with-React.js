8b4ef4b4770516af7e3a1674dcf4df8987689d86
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-10 17:30:28 +0300

    added footer component

---
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+    return (
+        <section className="section footer-section border border-danger">
+            <div className='container'>
+                <div className='row'>
+                <h2>Footer Section!</h2>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Footer;
