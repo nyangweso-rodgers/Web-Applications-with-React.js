d881bfc26543e281c07f353fb4b19140b3521e4c
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-11 17:39:32 +0300

    added component for survey header

---
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/SurveyHeader/SurveyHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SurveyHeader = (props) => {
+    return (
+        <header className='header-section'>
+            <nav className='navbar navbar-expand-lg'>
+                <div className='container'>
+                    <a className='navbar-brand'>Rodgers Nyangweso</a>
+                </div>
+            </nav>
+        </header>
+    )
+};
+
+export default SurveyHeader;
