2e186331b09f3f3f629ba3f6371b0f1e904ab6f4
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-11 17:38:50 +0300

    applied conditional rendering to App.js

--- ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
-import './App.css'
+import React, { useState} from "react";
+import './App.css';
+import SurveyHeader from './components/SurveyHeader/SurveyHeader';
+import Counter from "./components/Counter/Counter";
 import MyHeader from "./components/Header/MyHeader";
-import About from "./components/About/About";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Blog from "./components/Blog/Blog";
-import Footer from "./components/Footer/Footer";
+//import About from "./components/About/About";
+//import Portfolio from "./components/Portfolio/Portfolio";
+//import Blog from "./components/Blog/Blog";
+//import Footer from "./components/Footer/Footer";
 
 const App = () => {
+
+    // set the conditions for rendering various components
+    const shouldRenderSurveyHeader = true;
+    const shouldRenderMyHeader = false;
+    const shouldRenderCounter = true;
+
+
     return (
         <div className="app">
-            <MyHeader />
-            <About />
-            <Portfolio />
-            <Blog />
-            <Footer />
+            {/* Conditional Rendering for SurveyHeader */}
+            {shouldRenderSurveyHeader && <SurveyHeader />}
+            {shouldRenderMyHeader && <MyHeader />}
+            {shouldRenderCounter && <Counter />}
         </div>
     )
 };
