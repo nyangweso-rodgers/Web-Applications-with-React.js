76714f60e98aa905d0c56746b1574801bec3ce2c
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-15 23:18:09 +0300

    modified app.css and app.js

--- ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
@@ -1,27 +1,30 @@
-import React, { useState} from "react";
+import React from "react";
 import './App.css';
-import SurveyHeader from './components/SurveyHeader/SurveyHeader';
+import HeaderForMyWebPageApp from './components/HeaderForMyWebApp/HeaderForMyWebPageApp';
+import HeaderForSurveyApp from './components/HeaderForSurveyApp/HeaderForSurveyApp';
 import Counter from "./components/Counter/Counter";
-import MyHeader from "./components/Header/MyHeader";
 //import About from "./components/About/About";
 //import Portfolio from "./components/Portfolio/Portfolio";
 //import Blog from "./components/Blog/Blog";
 //import Footer from "./components/Footer/Footer";
+import SurveyBodyForMarketPricing from "./components/SurveyBodyForMarketPricing/SurveyBodyForMarketPricing"
 
 const App = () => {
 
     // set the conditions for rendering various components
-    const shouldRenderSurveyHeader = true;
-    const shouldRenderMyHeader = false;
-    const shouldRenderCounter = true;
+    const shouldRenderHeaderForMyWebPageApp = false;
+    const shouldRenderHeaderForSurveyApp = true;
+    const shouldRenderCounter = false;
+    const shouldRenderSurveyBodyForMarketPricing = true;
 
 
     return (
         <div className="app">
             {/* Conditional Rendering for SurveyHeader */}
-            {shouldRenderSurveyHeader && <SurveyHeader />}
-            {shouldRenderMyHeader && <MyHeader />}
+            {shouldRenderHeaderForSurveyApp && <HeaderForSurveyApp />}
+            {shouldRenderHeaderForMyWebPageApp && <HeaderForMyWebPageApp />}
             {shouldRenderCounter && <Counter />}
+            {shouldRenderSurveyBodyForMarketPricing && <SurveyBodyForMarketPricing />}
         </div>
     )
 };
