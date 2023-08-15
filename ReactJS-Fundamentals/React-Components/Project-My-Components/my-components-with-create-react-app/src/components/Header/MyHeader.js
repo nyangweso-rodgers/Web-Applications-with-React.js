e38b4e166509546c9e2ea2e8d9b19cac61119ca6
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-15 23:06:18 +0300

    modified component

--- ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/Header/MyHeader.js
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/Header/MyHeader.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './MyHeader.css'
 
 const MyHeader = () => {
     return (
         <header className="">
             <nav className="navbar navbar-expand-sm sticky-top">
                 <div className="container">
-                    <a className='navbar-brand' href='#myAboutSection'>Rodgers Nyangweso</a>
+                    <a className='navbar-brand' href='/#'>Rodgers Nyangweso</a>
                 </div>
             </nav>
         </header>
