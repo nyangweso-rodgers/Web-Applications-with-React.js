39bd32e1a6dcaeb5f10a23f2f7e9e60e3c5c9ad5
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-15 23:17:30 +0300

    added a header for survey component folder

---
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/HeaderForSurveyApp/HeaderForSurveyApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HeaderForSurveyApp = () => {
+    return (
+        <header className=''>
+            <nav className='navbar navbar-expand-sm sticky-top border'>
+                <div className='container'>
+                    <a className='navbar-brand border' href='/#'>Rodgers Nyangweso</a>
+                </div>
+            </nav>
+        </header>
+    )
+};
+
+export default HeaderForSurveyApp;
