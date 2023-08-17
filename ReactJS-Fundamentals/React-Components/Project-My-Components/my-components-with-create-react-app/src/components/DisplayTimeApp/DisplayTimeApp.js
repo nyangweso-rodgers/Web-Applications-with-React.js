d7a6327d192c76fd3457257f5584f48dc6b3a875
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-17 20:15:27 +0300

    modified displaytime component

---
+++ ReactJS-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/components/DisplayTimeApp/DisplayTimeApp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DisplayTimeApp = () => {
+    return (
+        <>
+            <header>
+                <nav className='navbar navbar-expand-md navbar-light bg-dark'>
+                    <div className='container'>
+                        <span className='navbar-brand mb-0'>Display Time App</span>
+                    </div>
+                </nav>
+            </header>
+            <main></main>
+            <footer></footer>
+        </>
+    )
+};
+
+export default DisplayTimeApp;
