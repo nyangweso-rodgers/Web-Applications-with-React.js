001503ae3425cce6f348a4d961779c3c49d02167
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-24 17:06:15 +0300

    added a components folder with initial commit on CounterApp component

---
+++ React-Fundamentals/React-Components/Project-My-Components/my-components-with-nextjs/src/app/components/CounterApp/CounterApp.js
@@ -0,0 +1,19 @@
+import './CounterApp.css';
+
+
+const CounterApp = () => {
+    return (
+        <>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div className="container">
+                        <a className="navbar-brand">Counter Application with <code>Next.js</code></a>
+                    </div>
+                </nav>
+            </header>
+            <section className="section"></section>
+        </>
+    )
+}
+
+export default CounterApp;
