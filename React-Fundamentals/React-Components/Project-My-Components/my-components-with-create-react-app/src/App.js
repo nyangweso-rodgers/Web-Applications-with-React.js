b9e0d5ed12ebcf8bb4a5c42fc3d0ae13bbd32148
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-24 10:47:29 +0300

    modified App.js

--- React-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
+++ React-Fundamentals/React-Components/Project-My-Components/my-components-with-create-react-app/src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 import './App.css';
 import WebsiteHeader from './components/Headers/WebsiteHeader/WebsiteHeader'
 import MarketSurveyForm from './components/Forms/MarketSurveyForm/MarketSurveyForm';
+import CounterApp from "./components/CounterApp/CounterApp";
+import DisplayTimeApp from "./components/DisplayTimeApp/DisplayTimeApp";
 
 const App = () => {
 
     // set the conditions for rendering various components
     const shouldRenderWebsiteHeader = true;
     const shouldRenderMarketSurveyForm = false;
+    const shouldRenderCounterApp = false;
+    const shouldRenderDisplayTimeApp = false;
 
     return (
         <div className="app">
             {/* Conditional Rendering for SurveyHeader */}
             {shouldRenderWebsiteHeader && <WebsiteHeader />}
             {shouldRenderMarketSurveyForm && <MarketSurveyForm />}
+            {shouldRenderCounterApp && <CounterApp />}
+            {shouldRenderDisplayTimeApp && <DisplayTimeApp />}
         </div>
     )
 };
