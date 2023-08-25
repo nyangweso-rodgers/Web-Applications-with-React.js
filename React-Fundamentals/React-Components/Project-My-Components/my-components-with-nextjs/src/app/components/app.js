4a801c8836f389c618efd32a9738a488eb274d13
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-25 17:08:42 +0300

    added app.js file for displaying the components

---
+++ React-Fundamentals/React-Components/Project-My-Components/my-components-with-nextjs/src/app/components/app.js
@@ -0,0 +1,12 @@
+import CounterApp from "./CounterApp/CounterApp";
+import MarketSurveyForm from "./Forms/MarketSurveyForm/MarketSurveyForm";
+import PricingTable from "./Tables/PricingTable/PricingTable";
+
+
+const App = () => {
+    return (
+        <PricingTable />
+    )
+};
+
+export default App;
