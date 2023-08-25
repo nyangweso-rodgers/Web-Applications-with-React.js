d98167ecb226d76b5dfc0b6cbfc923ff62d8130e
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-25 17:34:29 +0300

    styled the btn element of the component

--- React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/Headers/WebsiteHeader.js
+++ React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/Headers/WebsiteHeader.js
@@ -4,7 +4,7 @@ const WebsiteHeader = () => {
     return (
         <>
             <header className=''>
-                <nav className="navbar navbar-expand-md">
+                <nav className="navbar navbar-expand-md fixed-top">
                     <div className="container">
                         <a className="navbar-brand" href="#/">Rodgers Nyangweso</a>
                         <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navbarHeader">
@@ -33,17 +33,33 @@ const WebsiteHeader = () => {
                     </div>
                 </nav>
             </header>
-            <section className="section border border-primary">
+            <section className="section about-section border border-primary">
                 <div className="container border p-5">
-                    <div className="row text-center">
+                    <div className="row">
                         <h1>Section content goes here!</h1>
                     </div>
                 </div>
             </section>
             <section className="section border border-secondary">
-                <div className="container-fluid border border-warning p-5">
-                    <div className="row text-center">
+                <div className="container-fluid border border-warning">
+                    <div className="row border">
                         <h1>Another section content goes here!</h1>
+                    </div>
+                    <div className='row border'>
+                        <div className='col-sm card-col'>
+                            <div className='card'></div>
+                        </div>
+                        <div className='col-sm card-col'>
+                            <div className='card'></div>
+                        </div>
+                        <div className='col-sm card-col'>
+                            <div className='card'></div>
+                        </div>
+                    </div>
+                    <div className='row border text-center'>
+                        <div className='col'>
+                            <button className='btn'>More Articles</button>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -51,6 +67,11 @@ const WebsiteHeader = () => {
                 <div className="container border border-danger text-center">
                     <div className="row border border-info p-5">
                         <h2>This is a footer!</h2>
+                        <div className="row">
+                            <hr className='hr w-100' />
+                            <small className='text-center mt-3'>&copy; Rodgers Nyangweso, 2023. All rights reserved.</small>
+                        </div>
+                        
                     </div>
                 </div>
             </footer>
