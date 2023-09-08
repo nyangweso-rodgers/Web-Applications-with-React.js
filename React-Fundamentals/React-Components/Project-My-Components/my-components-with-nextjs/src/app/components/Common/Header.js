5dd8747b4538b184a6b898c4927aa4348abe76df
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-08 12:44:30 +0300

    added common folder for components being re-used

---
+++ React-Fundamentals/React-Components/Project-My-Components/my-components-with-nextjs/src/app/components/Common/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <nav className='navbar navbar-expand-md fixed-top'>
+                    <div className='container'></div>
+                </nav>
+            </header>
+        </>
+    )
+};
+
+export default Header;
