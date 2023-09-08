9258f5183594ba902666f6ede9f8984d986d50df
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-08 14:26:47 +0300

    added about components

---
+++ React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/About/About.js
@@ -0,0 +1,17 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <>
+      <main className="border border-primary">
+        <div className="container border border-secondary">
+          <div className="row">
+            <h2>About Section!</h2>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default About;
