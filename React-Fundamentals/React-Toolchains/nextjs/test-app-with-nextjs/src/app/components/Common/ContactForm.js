9824500587611941368308a102184c1ef1819aa3
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-08 22:20:03 +0300

    added contact forms component

---
+++ React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/Common/ContactForm.js
@@ -0,0 +1,17 @@
+import "./ContactForm.css";
+
+const ContactForm = () => {
+  return (
+    <>
+      <form className="border">
+        <div className="container">
+          <div className="row">
+            <h2>Contact form goes here</h2>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default ContactForm;
