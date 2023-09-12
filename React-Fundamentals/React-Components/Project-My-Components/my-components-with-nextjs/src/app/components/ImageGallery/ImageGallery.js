f44ed9112fbe8941a9462e5d03dc770d7b14baab
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-12 19:36:03 +0300

    finished ImageGallery component

--- React-Fundamentals/React-Components/Project-My-Components/my-components-with-nextjs/src/app/components/ImageGallery/ImageGallery.js
+++ React-Fundamentals/React-Components/Project-My-Components/my-components-with-nextjs/src/app/components/ImageGallery/ImageGallery.js
@@ -5,14 +5,14 @@ import MyPic from "../../../../public/images/20230323_122421.jpg";
 const ImageGallery = () => {
   return (
     <>
-      <section className="image-gallery-container">
-        <div className="row border">
+      <section className="image-gallery-section">
+        <div className="container image-gallery-container">
+          <div className="row">
             <h2>Image Gallery</h2>
             <span className="image-border"></span>
-        </div>
-        <div className="container border p-5">
-          <div className="d-flex flex-row justify-content-evenly">
-            <div>
+          </div>
+          <div className="row justify-content-evenly">
+            <div className="col img-col">
               <a className="" href="#">
                 <Image
                   src={MyPic}
@@ -23,30 +23,42 @@ const ImageGallery = () => {
                   alt="Rodgers Pic"
                 />
               </a>
+              <div>
+                <h3 className="name">Rodgers Nyangweso</h3>
+                <p>Founder and Chief Executive Officer</p>
+              </div>
             </div>
-            <div>
+            <div className="col img-col">
               <a className="" href="#">
                 <Image
                   src={MyPic}
                   width={380}
                   height={280}
-                  quality={80}
                   layout="responsive"
+                  quality={80}
                   alt="Rodgers Pic"
                 />
               </a>
+              <div>
+                <h3 className="name">Rodgers Nyangweso</h3>
+                <p>Co-Founder and Chief Technology Officer</p>
+              </div>
             </div>
-            <div>
+            <div className="col img-col">
               <a className="" href="#">
                 <Image
                   src={MyPic}
                   width={380}
                   height={280}
-                  quality={80}
                   layout="responsive"
+                  quality={80}
                   alt="Rodgers Pic"
                 />
               </a>
+              <div>
+                <h3 className="name">Rodgers Nyangweso</h3>
+                <p>Chief Operations Officer</p>
+              </div>
             </div>
           </div>
         </div>
