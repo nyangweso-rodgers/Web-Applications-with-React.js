8b1ca96a477388e54d25c19a2a1310b499618c8e
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-12 20:29:11 +0300

    added contact form section

--- React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/Common/ContactForm.js
+++ React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/Common/ContactForm.js
@@ -3,10 +3,39 @@ import "./ContactForm.css";
 const ContactForm = () => {
   return (
     <>
-      <form className="border">
-        <div className="container">
+      <form className="contact-form-section border">
+        <div className="container contact-form-container">
           <div className="row">
-            <h2>Contact form goes here</h2>
+            <h2>Contact me</h2>
+            <span className="contact-form-border"></span>
+          </div>
+          <div className="row">
+            <div className="contact-form">
+              <input
+                className="contact-form-text"
+                type="text"
+                placeholder="Your Name"
+              ></input>
+              <input
+                className="contact-form-text"
+                type="email"
+                placeholder="Email"
+              ></input>
+              <input
+                className="contact-form-text"
+                type="text"
+                placeholder="Your Phone"
+              ></input>
+              <textarea
+                className="contact-form-text"
+                placeholder="Your Message"
+              ></textarea>
+              <input
+                className="contact-form-btn"
+                type="submit"
+                value="Send"
+              ></input>
+            </div>
           </div>
         </div>
       </form>
