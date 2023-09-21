8e118e2e8fa73048924bda788c2ac4b5f0d3741a
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-21 23:41:12 +0300

    added blog component

---
+++ Building-React-Components-with-Next.js/website-with-next.js/src/app/Components/Blog/page.js
@@ -0,0 +1,15 @@
+const Blog = () => {
+  return (
+    <>
+      <section className="border border-primary">
+        <div className="container border border-secondary">
+          <div className="row border border-info">
+            <h1>Welcome to blog section!</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Blog;
