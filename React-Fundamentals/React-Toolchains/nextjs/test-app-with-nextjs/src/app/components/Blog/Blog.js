56e9aebcf59f66f1fb2946e64ede049cf4917342
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-22 22:14:29 +0300

    modified blog component

--- React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/Blog/Blog.js
+++ React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/Blog/Blog.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./Blog.css";
-import Link from "next/link";
-import FirstPost from "./FirstPost";
 
 const Blog = () => {
   return (
@@ -22,21 +20,6 @@ const Blog = () => {
             </div>
             <div className="col-sm card-col">
               <div className="card"></div>
-            </div>
-          </div>
-          <div className="row border border-success text-center">
-            <div className="col">
-              <button className="btn">Read more</button>
-            </div>
-          </div>
-          <div className="row text-center border border-danger">
-            <div className="col">
-              <button className="btn">Read more</button>
-            </div>
-          </div>
-          <div className="row text-center border border-warning">
-            <div className="col">
-              <Link href="/FirstPost">More articles</Link>
             </div>
           </div>
         </div>
