0a9ebe773d310391740345cd605823a5072b6c9c
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-25 20:47:39 +0300

    created Header component

---
+++ Building-React-Components-with-Next.js/react-components-with-next.js/src/app/Components/Common/Header/page.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <nav className="navbar border p-5">
+            <div className="container border p-5">
+                <a className="navbar-brand" href="#">Rodgers Nyangweso</a>
+            </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
