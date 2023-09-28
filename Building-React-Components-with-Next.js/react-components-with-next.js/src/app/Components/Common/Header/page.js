01b60c1efc004fd61783b6f630e72bf6214661f2
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-09-28 21:08:28 +0300

    added header skeleton component

--- Building-React-Components-with-Next.js/react-components-with-next.js/src/app/Components/Common/Header/page.js
+++ Building-React-Components-with-Next.js/react-components-with-next.js/src/app/Components/Common/Header/page.js
@@ -1,13 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header>
-        <nav className="navbar border p-5">
-            <div className="container border p-5">
-                <a className="navbar-brand" href="#">Rodgers Nyangweso</a>
-            </div>
-        </nav>
-      </header>
+      <header className="border p-5"></header>
     </>
   );
 };
