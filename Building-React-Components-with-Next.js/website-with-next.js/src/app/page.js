7c1098c4d080b738a92cdb293811cb757d4a6fa7
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-10-01 21:08:53 +0300

    imported the fontawesome library to the root page component

--- Building-React-Components-with-Next.js/website-with-next.js/src/app/page.js
+++ Building-React-Components-with-Next.js/website-with-next.js/src/app/page.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Link from "next/link";
 import styles from "./page.module.css";
 import Header from "./Components/Header/page";
