71d72a6686c1d64de8696164609c5072ba1a0c73
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-10-01 21:03:38 +0300

    imported fontawesome dev dependency

--- Building-React-Components-with-Next.js/react-components-with-next.js/src/app/page.js
+++ Building-React-Components-with-Next.js/react-components-with-next.js/src/app/page.js
@@ -1,10 +1,14 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import styles from "./page.module.css";
-import Header from "./Components/Common/Header/page.js"
+import Header from "./Components/Common/Header/page.js";
+import LoginForm from "./Components/Forms/LoginForm/page";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <LoginForm />
     </>
   );
 };
