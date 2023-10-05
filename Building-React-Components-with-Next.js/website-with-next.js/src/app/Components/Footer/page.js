274574f4ea8e789ff7d8b07b54a784895d2a0612
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-10-05 20:14:48 +0300

    added footer component and modified global.css file

--- Building-React-Components-with-Next.js/website-with-next.js/src/app/Components/Footer/page.js
+++ Building-React-Components-with-Next.js/website-with-next.js/src/app/Components/Footer/page.js
@@ -1,8 +1,39 @@
-
 const Footer = () => {
   return (
     <>
-      <footer className="border"></footer>
+      <footer className="">
+        <div className="container">
+          <div className="row">
+            <hr className="hr w-100" />
+          </div>
+          <div className="row">
+            <div className="col-sm-8 d-flex">
+              <div className="col-sm-6">
+                <small className="copyright">
+                  &copy; Rodgers Nyangweso, 2023. All rights reserved.
+                </small>
+              </div>
+              <div className="col-sm-3">
+                <small>Privacy Policy</small>
+              </div>
+              <div className="col-sm-3">
+                <small>Terms of Service</small>
+              </div>
+            </div>
+            <div className="col-sm-4 d-flex justify-content-evenly">
+              <div className="">
+                <span className="fab fa-linkedin-in fa-2x"></span>
+              </div>
+              <div className=" ">
+                <span className="fab fa-github fa-2x"></span>
+              </div>
+              <div className="">
+                <span className="fab fa-twitter fa-2x"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
