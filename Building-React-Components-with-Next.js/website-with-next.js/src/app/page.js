be27935044625d5222af95df0d8a9ed994f95981
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-18 12:09:10 +0300

    removed the ContactForm component from the root page.js

--- Building-React-Components-with-Next.js/website-with-next.js/src/app/page.js
+++ Building-React-Components-with-Next.js/website-with-next.js/src/app/page.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
+// import bootstrap to pages.js
+import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Link from "next/link";
 import styles from "./page.module.css";
 import Header from "./Components/Header/page";
 import About from "./Components/About/page";
 import Blog from "./Components/Blog/page";
-import ContactForm from "./Components/ContactForm/page";
 import Footer from "./Components/Footer/page";
 
 const Home = () => {
@@ -14,7 +15,6 @@ const Home = () => {
       <Header />
       <About />
       <Blog />
-      <ContactForm />
       <Footer />
     </>
   );
