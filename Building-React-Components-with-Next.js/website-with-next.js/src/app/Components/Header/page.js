ca5318dbcf026cf522940960bf26f58a5f417a10
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-19 22:11:44 +0300

    added styles for the header component

--- Building-React-Components-with-Next.js/website-with-next.js/src/app/Components/Header/page.js
+++ Building-React-Components-with-Next.js/website-with-next.js/src/app/Components/Header/page.js
@@ -1,10 +1,11 @@
 import "../../globals.css";
+import "./Header.css";
 
 const Header = () => {
   return (
     <>
       <header>
-        <nav className="border border-danger">
+        <nav className="navbar navbar-expand-md">
           <div className="container">
             <a className="navbar-brand" href="#">
               Rodgers Nyangweso
@@ -13,20 +14,58 @@ const Header = () => {
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
+              data-bs-target="#navbarCollapseContent"
+              aria-controls="#navbarCollapseContent"
               aria-expanded="false"
-              aria-label="Toggle navigation"
             >
               <span className="navbar-toggler-icon"></span>
             </button>
             <div
+              id="navbarCollapseContent"
               className="collapse navbar-collapse"
-              id="navbarSupportedContent"
             >
-              <ul className="navbar-nav">
+              <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">About</a>
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item dropdown mega-dropdown">
+                  <a
+                    href="#"
+                    id="navbarDropdown"
+                    className="nav-link dropdown-toggle"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                  >
+                    Blog
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                    data-bs-toggle="dropdown"
+                  >
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        An item
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        A second item
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        A third item
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
                 </li>
               </ul>
             </div>
