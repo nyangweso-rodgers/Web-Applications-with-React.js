c55c71edf7167765c5db2fa8ead88b3842de6aa0
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-19 20:56:50 +0300

    added more html elements to the Header component

--- Building-React-Components-with-Next.js/react-components-with-next.js/src/app/Components/Common/Header/page.js
+++ Building-React-Components-with-Next.js/react-components-with-next.js/src/app/Components/Common/Header/page.js
@@ -1,7 +1,18 @@
 const Header = () => {
   return (
     <>
-      <header className="border p-5"></header>
+      <header className="border border-danger p-5">
+        <nav className="navbar navbar-expand-md">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              Edit Header Component
+            </a>
+            <button>
+              <span></span>
+            </button>
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
