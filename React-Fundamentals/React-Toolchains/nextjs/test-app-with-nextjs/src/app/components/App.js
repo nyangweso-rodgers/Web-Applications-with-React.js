7aa70a5153090f6ddbfd3a390c8d0dd79e805083
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-19 21:38:21 +0300

    modified App.js and About.js components

--- React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/App.js
+++ React-Fundamentals/React-Toolchains/nextjs/test-app-with-nextjs/src/app/components/App.js
@@ -1,17 +1,11 @@
 import Header from "./Common/Header";
 import About from "./Common/About";
-import Blog from "./Blog/Blog";
-import ContactForm from "./Common/ContactForm";
-import Footer from "./Common/Footer";
 
 const App = () => {
     return (
       <>
         <Header />
         <About />
-        <Blog />
-        <ContactForm />
-        <Footer />
       </>
     );
 };
