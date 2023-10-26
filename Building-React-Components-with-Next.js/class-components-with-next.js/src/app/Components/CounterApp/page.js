a92daa58be58f4f047a6c5f2803c161efb7a0d17
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-26 23:24:12 +0300

    added Components folder with initial commit on CounterApp class component

---
+++ Building-React-Components-with-Next.js/class-components-with-next.js/src/app/Components/CounterApp/page.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react"; // Create class-based React components.
+
+// class Component
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter: {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp; // Exports the Counter component to make it accessible.
