ba5f9e1b93016e000bd07b850e415b9ab66e8742
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-30 22:54:41 +0300

    imported the ParentComponent to the root

--- React-Components-with-Next.js/functional-components-with-next.js/src/app/page.js
+++ React-Components-with-Next.js/functional-components-with-next.js/src/app/page.js
@@ -7,11 +7,12 @@ import LoginForm from "./Components/Forms/LoginForm/page";
 import Footer from "./Components/Common/Footer/page";
 import Counter from "./Components/Counter/page";
 import CardTemplate from "./Components/Cards/CardTemplate/page.js";
+import ParentComponent from "./Components/ParentComponent/page.js";
 
 const Home = () => {
   return (
     <>
-    <CardTemplate />
+    <ParentComponent />
     </>
   );
 };
