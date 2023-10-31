c66d1fa62ece097a39854375f8ec48474670d7bd
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-10-31 22:38:47 +0300

    imported useEffect hook

--- React-Components-with-Next.js/my-website-with-next.js/src/app/page.js
+++ React-Components-with-Next.js/my-website-with-next.js/src/app/page.js
@@ -1,3 +1,5 @@
+"use client"; // This is a client component
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 // import bootstrap to pages.js
 import "bootstrap/dist/css/bootstrap.css";
@@ -8,6 +10,9 @@ import Blog from "./Components/Blog/page";
 import Footer from "./Components/Footer/page";
 
 const MyWebsite = () => {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   return (
     <>
       <Header />
