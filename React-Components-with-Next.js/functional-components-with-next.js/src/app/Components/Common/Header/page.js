d266f76e2f6a50547b2fdf6825c856c5114827f4
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-01 22:02:11 +0300

    used the useState hook

--- React-Components-with-Next.js/functional-components-with-next.js/src/app/Components/Common/Header/page.js
+++ React-Components-with-Next.js/functional-components-with-next.js/src/app/Components/Common/Header/page.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [myFullName, setMyFullName] = useState("Rodgers Nyangweso");
   return (
     <>
-      <header className="border border-danger p-5">
+      <header className="border border-danger">
         <nav className="navbar navbar-expand-md">
           <div className="container">
             <a className="navbar-brand" href="#">
-              Edit Header Component
+              {myFullName}
             </a>
             <button>
               <span></span>
